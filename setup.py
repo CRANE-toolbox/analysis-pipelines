e6593843a85833766b8130ecc93665eaa3b17d67
Ian Saucy <iansaucy@gmail.com>
2020-07-11 23:39:33 -0400

    Structure for summer project

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="CRANE-Toolbox",
+    version="0.0.1",
+    author="The CRANE Team",
+    author_email="author@example.com",
+    description="A package against COVID-19",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CRANE-toolbox/analysis-pipelines",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
